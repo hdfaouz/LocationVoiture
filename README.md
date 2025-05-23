9e88129114eab0506e83a5e75a8c0bae6f31b5b2
hdfaouz <hdfaouzia2@gmail.com>
2025-05-23 17:29:34 +0100

    Diagrmme de cas d'utilisation

---
+++ README.md
@@ -0,0 +1,2 @@
+**_Diagramme de cas d'utilisation_**
+![Diagrammes de cas d'utilisation](Diagrammes/diagramme%20de%20cas%20d'utilisation.png)
---
+++ src/test/java/com/enaa/locatiovoitures/LocationDesVoituresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enaa.locatiovoitures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationDesVoituresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
