84b56510c2815aa5be8508c4b60c3674845f02e1
hdfaouz <hdfaouzia2@gmail.com>
2025-07-25 00:18:29 +0100

    injection des Classes dans ReservationTest

--- src/test/java/com/enaa/locatiovoitures/ReservationTest.java
+++ src/test/java/com/enaa/locatiovoitures/ReservationTest.java
@@ -1,4 +1,47 @@
 package com.enaa.locatiovoitures;
 
+import com.enaa.locatiovoitures.Dto.ReservationDto;
+import com.enaa.locatiovoitures.Mappers.ReservationMap;
+import com.enaa.locatiovoitures.Model.Client;
+import com.enaa.locatiovoitures.Model.Reservation;
+import com.enaa.locatiovoitures.Model.Voiture;
+import com.enaa.locatiovoitures.Repositories.ReservationRepository;
+import com.enaa.locatiovoitures.Repositories.UserRepository;
+import com.enaa.locatiovoitures.Repositories.VoitureRepository;
+import com.enaa.locatiovoitures.Services.ReservationService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class ReservationTest {
+    @Mock
+    private ReservationRepository reservationRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private VoitureRepository voitureRepository;
+
+    @Mock
+    private ReservationMap reservationMap;
+
+    @InjectMocks
+    private ReservationService reservationService; // Remplacez par le nom de votre service
+
+
 }
