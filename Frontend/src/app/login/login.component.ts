ee8477e4608f1e1c32de55712154d48b4c9c1104
hdfaouz <hdfaouzia2@gmail.com>
2025-08-05 12:54:22 +0100

    component login

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -7,6 +10,78 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm: FormGroup;
+  loading = false;
+  errorMessage = '';
+  successMessage = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit(): void {
+    this.errorMessage='';
+    this.successMessage='';
+
+  }
+
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
+
+
+  onSumbit():void{
+    this.errorMessage='';
+    this.successMessage='';
+    this.loading=true;
+
+    if (this.loginForm.valid) {
+
+      console.log(this.loginForm.value);
+      const dataLogin = this.loginForm.value;
+
+      this.authService.login(dataLogin).subscribe({
+
+        next: (response) => {
+          this.authService.saveUserData(response);
+          // this.snackBar.open('login successful','close',{duration:4000});
+          // this.router.navigate(['/parkings']);
+
+          this.authService.saveToken(response.token);
+          this.authService.saveRole(response.user.role);
+          console.log('FULL LOGIN RESPONSE:', response.user);
+
+          console.log('saved token : ', response.token);
+          console.log('user  is:', response.user);
+
+          switch(response.user.role) {
+            case 'ADMIN':
+              this.router.navigate(['/admin-dashboard']);
+              break;
+
+            case 'CUSTOMER':
+              this.router.navigate(['/customer-Dash']);
+              break;
+            default:
+              this.router.navigate(['/']);
+          }
+        },
+        error: (err)=>{
+          this.errorMessage = typeof err === 'string' ? err : 'Login failed';
+        }
+      })
+    }
+
+
+
+  }
+
 
 }
