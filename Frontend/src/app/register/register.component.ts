13f611356b6c7fee7d80ea3db28d6a3b53be5cb0
hdfaouz <hdfaouzia2@gmail.com>
2025-08-05 12:47:46 +0100

    travailler sur component de register et de authService

--- Frontend/src/app/register/register.component.ts
+++ Frontend/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService, RegisterRequest} from "../services/auth.service";
+import {Router} from "@angular/router"
 
 @Component({
   selector: 'app-register',
@@ -7,6 +10,50 @@ import { Component } from '@angular/core';
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  registerForm : FormGroup;
+  loading = false;
+  errorMessage = '';
+  successMessage = '';
+
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router){
+    this.registerForm= this.formBuilder.group({
+      name:['',[Validators.required]],
+      email:['',[Validators.required]],
+      password:['',[Validators.required]],
+      role:['',[Validators.required]]
+    });
+
+
+  }
+  ngOnInit(): void {
+    this.errorMessage='';
+    this.successMessage='';
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      const data :  RegisterRequest= this.registerForm.value;
+      this.authService.register(data).subscribe({
+        next : (response) =>{
+          this.successMessage = 'Registration successful';
+
+          console.log('user:',response)
+          this.errorMessage = '';
+          this.registerForm.reset();
+
+          setTimeout(()=> this.router.navigate(['/login']),1500);
+        },
+        error: err=>{
+          this.errorMessage = err || 'Registration failed';
+          this.successMessage='';
+        }
+      });
+
+    }
+  }
 
 }
