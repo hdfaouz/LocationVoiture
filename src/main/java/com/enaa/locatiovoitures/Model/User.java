9c045fc9bbcdc36787383975baf0943a00cdc073
hdfaouz <hdfaouzia2@gmail.com>
2025-08-08 15:05:46 +0100

    modification dans UserEntity

--- src/main/java/com/enaa/locatiovoitures/Model/User.java
+++ src/main/java/com/enaa/locatiovoitures/Model/User.java
@@ -2,7 +2,6 @@ package com.enaa.locatiovoitures.Model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
