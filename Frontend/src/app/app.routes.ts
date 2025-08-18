bc83642b450523de1f7f726b235c018fb50e605f
hdfaouz <hdfaouzia2@gmail.com>
2025-08-18 17:59:14 +0100

    creation de booking Component

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,11 +3,14 @@ import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {HomeComponent} from "./home/home.component";
 import {AddComponent} from "./Voiture/add/add.component";
+import {B} from "@angular/cdk/keycodes";
+import {BookingComponent} from "./booking/booking.component";
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path : 'register' , component: RegisterComponent},
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {path:'home', component: HomeComponent},
-  {path:'add', component:AddComponent}
+  {path:'add', component:AddComponent},
+  {path:'book',component: BookingComponent}
 ];
