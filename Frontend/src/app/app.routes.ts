6985ef3258d927663408d0828ab87d3e21d5f2e8
hdfaouz <hdfaouzia2@gmail.com>
2025-08-31 20:55:58 +0100

    Component Contact Us

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,16 +3,17 @@ import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {HomeComponent} from "./home/home.component";
 import {AddComponent} from "./Voiture/add/add.component";
-import {B} from "@angular/cdk/keycodes";
 import {BookingComponent} from "./booking/booking.component";
 import {DetailsComponent} from "./details/details.component";
+import {ContactUsComponent} from "./contact-us/contact-us.component";
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path : 'register' , component: RegisterComponent},
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  {path:'home', component: HomeComponent},
-  {path:'add', component:AddComponent},
-  {path:'book',component: BookingComponent},
-  {path:'detail',component:DetailsComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'add', component:AddComponent},
+  {path: 'book',component: BookingComponent},
+  {path: 'detail',component:DetailsComponent},
+  {path:'contact', component:ContactUsComponent}
 ];
--- Frontend/src/app/contact-us/contact-us.component.ts
+++ Frontend/src/app/contact-us/contact-us.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
