38aa8ea34d38be1b489595fdc9fbfbffc71a62a8
hdfaouz <hdfaouzia2@gmail.com>
2025-08-31 19:42:46 +0100

    modification dans tous les components

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import {HomeComponent} from "./home/home.component";
 import {AddComponent} from "./Voiture/add/add.component";
 import {B} from "@angular/cdk/keycodes";
 import {BookingComponent} from "./booking/booking.component";
+import {DetailsComponent} from "./details/details.component";
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -12,5 +13,6 @@ export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {path:'home', component: HomeComponent},
   {path:'add', component:AddComponent},
-  {path:'book',component: BookingComponent}
+  {path:'book',component: BookingComponent},
+  {path:'detail',component:DetailsComponent}
 ];
--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgOptimizedImage
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
