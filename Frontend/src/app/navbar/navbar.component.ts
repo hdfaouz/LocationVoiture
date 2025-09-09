3ef0e0caf018d5cc399479a11db212809f633f95
hdfaouz <hdfaouzia2@gmail.com>
2025-09-09 10:29:57 +0100

    fonction de logout

--- Frontend/src/app/navbar/navbar.component.ts
+++ Frontend/src/app/navbar/navbar.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive} from "@angular/router";
+import {Router, RouterLink, RouterLinkActive} from "@angular/router";
+import {AuthService} from "../services/auth.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    NgIf
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent{
+  constructor(public authService: AuthService, private router: Router) {}
 
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
